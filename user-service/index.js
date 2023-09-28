004c2e884fa10be0cce318e098c492ea1908b0ff
mozzam123 <mozzam607@gmail.com>
2023-09-28 16:37:31 +0530

    added db connection code

--- user-service/index.js
+++ user-service/index.js
@@ -1,7 +1,19 @@
 const app = require("./app")
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established'));
 
 
-
+// START SERVER
 app.listen(7800, () => {
     console.log(`Running on port 7800`);
 });
