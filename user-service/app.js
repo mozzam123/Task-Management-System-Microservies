2f4ee89c5c8038660ae40c5a143769adc51fdb23
mozzam123 <mozzam607@gmail.com>
2023-10-01 20:18:53 +0530

    minor changes

--- user-service/app.js
+++ user-service/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 const userroutes = require("./routes/userRoute");
+const path = require("path");
+const bodyParser = require("body-parser")
 
-app.use("/", userroutes);
+
+// Use body-parser middleware to parse URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Define template path
+const template_path = path.join(__dirname, "views");
+
+
+// Serve static files from public directory
+app.use(express.static("public"));
+
+// Set view engine and template path
+app.set("view engine", "hbs");
+app.set("views", template_path);
+app.set("views", path.join(__dirname, "views"));
+
+// Routes
+app.use(userroutes);
+
 module.exports = app;
--- user-service/controllers/userController.js
+++ user-service/controllers/userController.js
@@ -1,3 +1,13 @@
-exports.GetUser = (req, res) => {
-  res.send("mozzam from conrollers");
+exports.GetRegisterUser = (req, res) => {
+  res.render("registration");
+};
+
+exports.RegisterUser = async (req, res) => {
+  try {
+    console.log(req.body);
+    console.log("ran the post thing first*******************");
+    res.redirect("http://127.0.0.1:8000/register");
+  } catch (error) {
+    console.log(error);
+  }
 };
