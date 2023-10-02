2960bb1b6c5b65a7e9aa98eb9104c3d73a67358f
mozzam123 <mozzam607@gmail.com>
2023-10-02 18:41:15 +0530

    made register route resilient and added login page

--- user-service/controllers/userController.js
+++ user-service/controllers/userController.js
@@ -1,13 +1,36 @@
+const userModel = require("./../models/userModel");
+
 exports.GetRegisterUser = (req, res) => {
   res.render("registration");
 };
 
 exports.RegisterUser = async (req, res) => {
   try {
-    console.log(req.body);
-    console.log("ran the post thing first*******************");
-    res.redirect("http://127.0.0.1:8000/register");
+    const username = req.body.username;
+    const existingUser = await userModel.findOne({username: username});
+    const alreadyEmail = await userModel.findOne({email: req.body.email})
+    if (existingUser || alreadyEmail) {
+      res.render("registration", { message: "Username or email already exists" });
+    } else {
+      const userData = new userModel({
+        username: username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      const savedData = await userData.save();
+      console.log(
+        `New user saved with usename: ${savedData.username} and password: ${savedData.password}`
+      );
+      res.render("login");
+    }
   } catch (error) {
-    console.log(error);
+    console.log("*****errors*****", error);
+    res.render("registration", { error: error.message });
   }
 };
+
+
+exports.GetLoginPage = async (req,res) =>{
+  res.render("login")
+}
--- user-service/routes/userRoute.js
+++ user-service/routes/userRoute.js
@@ -4,5 +4,7 @@ const userController = require("./../controllers/userController");
 
 router.route("/register").get(userController.GetRegisterUser);
 router.route("/register").post(userController.RegisterUser);
+router.route("/login").get(userController.GetLoginPage);
+
 
 module.exports = router;
