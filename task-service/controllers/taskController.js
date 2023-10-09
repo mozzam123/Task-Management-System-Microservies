363034b16ebb36515cb7c7b3fe82a4f9367c51cb
mozzam123 <mozzam607@gmail.com>
2023-10-09 15:03:40 +0530

    added few changes in hbs

--- task-service/controllers/taskController.js
+++ task-service/controllers/taskController.js
@@ -1,32 +1,32 @@
 const taskModel = require("./../models/taskModel");
-const { Kafka } = require("kafkajs");
+// const { Kafka } = require("kafkajs");
 
-// Create Kafka consumer instance
-const kafka = new Kafka({
-  clientId: "task-service",
-  brokers: ["localhost:9092"],
-});
+// // Create Kafka consumer instance
+// const kafka = new Kafka({
+//   clientId: "task-service",
+//   brokers: ["localhost:9092"],
+// });
 
-const consumer = kafka.consumer({
-  groupId: "task-group",
-  maxWaitTimeInMs: 50,
-});
+// const consumer = kafka.consumer({
+//   groupId: "task-group",
+//   maxWaitTimeInMs: 50,
+// });
 
-consumer.connect();
-console.log("Connected to consumer");
+// consumer.connect();
+// console.log("Connected to consumer");
 
-consumer.subscribe({ topic: "user-credentials" });
-console.log(`Subscribed to topic`);
+// consumer.subscribe({ topic: "user-credentials" });
+// console.log(`Subscribed to topic`);
 
 let latestUsername;
 
-consumer.run({
-  eachMessage: async ({ message }) => {
-    const { username } = JSON.parse(message.value.toString());
-    latestUsername = username;
-    console.log(`Received message with username: ${username}`);
-  },
-});
+// consumer.run({
+//   eachMessage: async ({ message }) => {
+//     const { username } = JSON.parse(message.value.toString());
+//     latestUsername = username;
+//     console.log(`Received message with username: ${username}`);
+//   },
+// });
 
 exports.GetTask = async (req, res) => {
   res.render("task", { currentUser: latestUsername });
