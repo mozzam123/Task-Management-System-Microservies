f34313c1e7a381e91d08e2cfff201ee6698eff9c
mozzam123 <mozzam607@gmail.com>
2023-10-12 15:58:20 +0530

    deleted one microservice

--- all-task-service/routes/allTaskRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const allTaskController = require("./../controllers/allTaskController")
-
-
-router.route('/all').get(allTaskController.GetAllTaskPage)
-
-
-module.exports = router
