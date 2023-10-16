89530a3d7036fac58315633237618c61407fa340
mozzam123 <mozzam607@gmail.com>
2023-10-16 16:27:49 +0530

    changes made in folder structure

---
+++ task-service/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config()
+const DB = process.env.DATABASE
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connection established for Task service'));
--- task-service/index.js
+++ task-service/index.js
@@ -1,18 +1,12 @@
-const mongoose = require('mongoose');
 const app = require("./app")
 const dotenv = require('dotenv');
-dotenv.config()
-const DB = process.env.DATABASE
+
 const PORT = process.env.PORT || 2000
+const connectDB = require("./config/database")
 
+// Connect DATABASE
+connectDB;
 
-
-mongoose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log('DB connection established for Task service'));
 
 app.listen(PORT, () => {
     console.log(`app listenting on http://localhost:${PORT}/`);
